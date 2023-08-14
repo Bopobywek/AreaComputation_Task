aa0791268eafdf52c6c1f4ba75c55ce5ff742042
andrey <anechesov1@gmail.com>
2023-08-14 13:05:27 +0300

    Добавил ответ на 3 вопрос

---
+++ Task_SQL.sql
@@ -0,0 +1,10 @@
+﻿/*
+    Ответ на вопрос 3
+ */
+
+SELECT products.name, categories.name
+FROM products
+         LEFT JOIN products_categories
+                   ON products.id = products_categories.product_id
+         LEFT JOIN categories
+                   ON products_categories.category_id = categories.id
